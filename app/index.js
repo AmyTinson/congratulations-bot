1e7b3df9cb1e79648fbfa67770ea463e5f87e52b
Amy Clarke <amyelizabethtinson@gmail.com>
2021-05-29 13:05:07 -0400

    stuff

--- app/index.js
+++ app/index.js
@@ -2,3 +2,19 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
+// Bot set-up
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.once('ready', () => {
+  console.log('Ready!')
+})
+
+client.login(process.env.TOKEN)
+
+// The actual fun BOT stuffs
+client.on('message', message => {
+  if (message.content === '!congrats') {
+    message.channel.send('https://youtu.be/1Bix44C1EzY')
+  }
+})
