7ff87d229f656f2b35322fba1fa98ffb62b940bc
Amy Tinson <amyelizabethtinson@gmail.com>
2021-09-29 19:25:38 -0400

    quick update to limit

--- app/funcs/getRandomTenorGifs.js
+++ app/funcs/getRandomTenorGifs.js
@@ -3,7 +3,7 @@
 const axios = require('axios')
 
 const getRandomTenorGifs = async (query, key) => {
-  const randomTenorGifsRequest = await axios(`https://g.tenor.com/v1/random?q=${query}&key=${key}&limit=3`)
+  const randomTenorGifsRequest = await axios(`https://g.tenor.com/v1/random?q=${query}&key=${key}&limit=50`)
   const randomTenorGifs = await randomTenorGifsRequest.data.results
   return randomTenorGifs
 }
